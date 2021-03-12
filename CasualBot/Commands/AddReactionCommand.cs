8dd044c37951551d69bd1da32862c951b5bde6ad
Pawaox <pawaox@gmail.com>
2021-03-12 12:03:14 +0100

    whoops, accidentally removed AddReactionCommand logic

--- CasualBot/Commands/AddReactionCommand.cs
+++ CasualBot/Commands/AddReactionCommand.cs
@@ -16,7 +16,47 @@ namespace CasualBot.Commands
 
         public override bool HandleCommand(PawDiscordBotClient client, SocketUserMessage msg, string[] levels, string[] parameters)
         {
-            throw new NotImplementedException();
+            bool handled = false;
+
+            if (levels.Length != 1)
+                return handled;
+
+            if (Settings.ParameterCount != parameters.Length)
+                return handled;
+
+            if (msg.Author is SocketGuildUser)
+            {
+                SocketGuildUser socketUser = (SocketGuildUser)msg.Author;
+                foreach (var role in socketUser.Roles)
+                {
+                    if (role.Name.Equals("Mod"))
+                    {
+                        var messagetxt = parameters[0];
+                        var reactionEmote = parameters[1];
+                        ulong messageID = 0;
+
+                        if (ulong.TryParse(messagetxt, out messageID))
+                        {
+                            var messagetarget = msg.Channel.GetMessageAsync(messageID).Result;
+                            if (messagetarget != null)
+                            {
+                                IEmote parsedEmote = null;
+                                if (Emote.TryParse(reactionEmote, out var emote))
+                                    parsedEmote = emote;
+                                else
+                                    parsedEmote = new Emoji(reactionEmote);
+
+                                messagetarget.AddReactionAsync(parsedEmote);
+
+                                msg.DeleteAsync();
+                            }
+                        }
+                    }
+                }
+            }
+
+            handled = true;
+            return handled;
         }
     }
 }
