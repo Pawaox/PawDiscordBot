7e66a5d86c0bd1b47071b82cac11ffddbb11e158
Pawaox <pawaox@gmail.com>
2021-12-18 19:07:38 +0100

    Initial Commit for GnomeParsingBot

---
+++ GnomeParsingBot/Commands/CreatePostCommand.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.WebSocket;
+using PawDiscordBot;
+using PawDiscordBot.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GnomeParsingBot.Commands
+{
+    public class CreatePostCommand : ParameterCommand
+    {
+        public CreatePostCommand() : base(new CommandSettings("createPost", 1)) { }
+
+        public override bool HandleCommand(PawDiscordBotClient client, SocketUserMessage msg, string[] levels, string[] parameters)
+        {
+            bool handled = false;
+
+            if (levels.Length != 1)
+                return handled;
+
+            if (Settings.ParameterCount != parameters.Length)
+                return handled;
+
+            if (msg.Author is SocketGuildUser)
+            {
+                SocketGuildUser socketUser = (SocketGuildUser)msg.Author;
+
+            }
+
+            handled = true;
+            return handled;
+        }
+    }
+}
---
+++ GnomeParsingBot/Program.cs
@@ -0,0 +1,74 @@
+﻿using Discord.WebSocket;
+using GnomeParsingBot;
+
+try
+{
+    string botKeyPath = "C:/discordclient.txt";
+    string botLogPath = "C:/discordBotLog.txt";
+    bool useWS4NetProvider = true;
+
+    string botKey = "";
+    if (File.Exists(botKeyPath))
+        botKey = File.ReadAllText(botKeyPath);
+    else
+        throw new Exception("Couldn't find client key at " + botKeyPath);
+
+    DiscordSocketConfig dsCfg = new DiscordSocketConfig();
+    if (useWS4NetProvider)
+        dsCfg.WebSocketProvider = Discord.Net.Providers.WS4Net.WS4NetProvider.Instance;
+
+    GnomeParsingBotClient client = new GnomeParsingBotClient(botKey);
+    client.Logger = new GnomeParsingBotLogger(botLogPath, true);
+
+    client.Start(dsCfg);
+
+    #region Console Input
+    ConsoleColor oldColor = Console.ForegroundColor;
+    bool doRun = true;
+    while (doRun)
+    {
+        string input = Console.ReadLine();
+
+        switch (input?.ToLower() ?? "")
+        {
+            case "stop":
+                doRun = false;
+
+                oldColor = ChangeForeground(ConsoleColor.Yellow);
+                client.Logger.Log("Input '" + input + "' - STOPPING...");
+                break;
+            case "exit":
+                doRun = false;
+
+                oldColor = ChangeForeground(ConsoleColor.Yellow);
+                client.Logger.Log("Input '" + input + "' - STOPPING...");
+                break;
+        }
+    }
+    #endregion
+
+    ChangeForeground(oldColor);
+    Thread.Sleep(4000);
+}
+catch (Exception exc)
+{
+    Console.WriteLine(exc.ToString());
+    Console.Write("Finished, Press <ENTER> to lose");
+    Console.ReadLine();
+}
+
+
+#region Console Helper Methods
+static ConsoleColor ChangeForeground(ConsoleColor newColor)
+{
+    ConsoleColor oldColor = Console.ForegroundColor;
+    Console.ForegroundColor = newColor;
+    return oldColor;
+}
+static ConsoleColor ChangeBackground(ConsoleColor newColor)
+{
+    ConsoleColor oldColor = Console.BackgroundColor;
+    Console.BackgroundColor = newColor;
+    return oldColor;
+}
+#endregion
